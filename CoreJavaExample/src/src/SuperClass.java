588d415afdfbf11545506f5f0928670eaaf388f1
SureshKasala <32533871+SureshKasala@users.noreply.github.com>
2017-10-05 12:16:25 +0530

    Update SuperClass.java

--- CoreJavaExample/src/src/SuperClass.java
+++ CoreJavaExample/src/src/SuperClass.java
@@ -3,6 +3,7 @@ package src;
 public class SuperClass {
 
 	public static void main(String[] args) {
+		//Suresh
 		System.out.println("Hello Suresh , This is my first application i committed into github");
 
 	}
