239f8fb91b596d051cc0a0e707cab541937752e1
Suresh Kasala <skasala1@in.ibm.com>
2017-10-05 11:45:27 +0530

    This is my first commit

---
+++ CoreJavaExample/src/src/SuperClass.java
@@ -0,0 +1,10 @@
+package src;
+
+public class SuperClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Suresh , This is my first application i committed into github");
+
+	}
+
+}
